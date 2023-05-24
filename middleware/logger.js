28bb891f48926722b3f30e25570ab9e3cba459e4
amoeed303 <200842@students.au.edu.pk>
2023-05-24 22:48:55 +0500

    added mongodb connection

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,17 +1,18 @@
 const { format } = require("date-fns");
 const { v4: uuid } = require("uuid");
 const fs = require("fs");
-const fsPromises = require("fs").promise;
+const fsPromises = require("fs").promises;
 const path = require("path");
 
 const logEvents = async (message, logFileName) => {
-  const dateTime = `${format(new Date(), "ddMMyyyy\tHH:mm:ss")}`;
-  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`
 
   try {
     if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
       await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
     }
+
     await fsPromises.appendFile(
       path.join(__dirname, "..", "logs", logFileName),
       logItem
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { logger, logEvents } = require("./middleware/logger");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
+console.log(process.env.NODE_ENV);
+connectDB();
+
+app.use(logger);
+//cross origin resourse sharing allows other resurses to access this REST API
+//CorsOptions allows only the urls mentioned in config/corsOPtion fille
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/", express.static(path.join(__dirname, "/public")));
 //static middle ware -built in removing it it will work
@@ -20,5 +38,15 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 not Found");
   }
 });
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT} `));
+app.use(errorHandler);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDb");
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT} `));
+});
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
